90184bd5215732a9a32e17903c5f7637e08b1b01
GiovannyBrugge <giovannybrugge@live.nl>
2022-09-30 03:02:00 +0200

    Added player movement
    Added player movement: walking left,right, jumping, facing a direction and collision.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float playerMovementSpeed = 225f;
+    private float jumpPower = 250f;
+    private float horizontalInput;
+    private Rigidbody2D playerRB;
+    private bool rightFace = true;
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Makes a float with the input "Horizontal" (Horizontal contains: A and LeftArrow for negative x movement; D and RightArrow for positive x movement)
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        playerRB.velocity = new Vector2(horizontalInput * playerMovementSpeed * Time.deltaTime, playerRB.velocity.y) ;
+     
+        if (rightFace == false && horizontalInput > 0)
+        {
+            Mirror();
+        }
+        else if(rightFace == true && horizontalInput < 0)
+        {
+            Mirror();
+        }
+    }
+    private void Update()
+    {
+        //Makes the play jump with the input: spacebar and W
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            playerRB.velocity = Vector2.up * jumpPower * Time.deltaTime;
+        }
+    }
+    //The player will face the correct direction
+    private void Mirror() 
+    {
+        rightFace = !rightFace;
+        Vector2 Size = transform.localScale;
+        Size.x *= -1;
+        transform.localScale = Size;
+    }
+
+    
+  
+}
