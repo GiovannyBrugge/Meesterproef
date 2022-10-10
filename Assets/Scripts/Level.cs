79d68693c906db4fa96e65d358374bc29c6b260a
GiovannyBrugge <giovannybrugge@live.nl>
2022-10-10 21:24:46 +0200

    Fixed player movement
    Player movement while the player is rotated is now fixed

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Player player;
+    private Camera segmentCamera;
+    private readonly Vector3[] segmentCameraPositions =
     {
+     new Vector3(0.0f,0.0f,-10f),
+     new Vector3(-18f,0.0f,-10f)
+    };
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        segmentCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+    }
+    //Detects other obstacles
+    public void PlayerCollision(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Ravine":
+                player.Die();
+                Debug.Log("I touched a ravine");
+                break;
+            case "Spike":
+                player.Die();
+                Debug.Log("I touched a spike");
+                break;
+        }
+    }
+    //Checks for trigger to change the camera position
+    public void PlayerCollider(Collider2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "1to2":
+                segmentCamera.transform.position = segmentCameraPositions[1];
+                break;
+        }
         
+        //Check gravity zone
+        switch (other.gameObject.name)
+        {
+            case "GravityUp":
+                Physics2D.gravity = new Vector2(0f, 9.81f);
+                player.Rotate("Up");
+                break;
+
+            case "GravityDown":
+                Physics2D.gravity = new Vector2(0f, -9.81f);
+                player.Rotate("Down");
+                break;
+
+            case "GravityRight":
+                Physics2D.gravity = new Vector2(9.81f, 0f);
+                player.Rotate("Right");
+                break;
+
+            case "GravityLeft":
+                Physics2D.gravity = new Vector2(-9.81f, 0f);
+                player.Rotate("Left");
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,41 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Camera segmentCamera;
-    
-    //index 0 = Segement 1, index 1 = segment 2 etc. etc.
-    readonly Vector3[] segmentCameraPositions =
-     {
-     new Vector3(0.0f,0.0f,-10f),
-     new Vector3(-18f,0.0f,-10f)
-    };
-    
+    [HideInInspector]
+    public Level level;
+    private Rigidbody2D playerRB;
+    private Transform groundCheck;
+    public LayerMask groundLayer;
+    const float checkCollisionRadius = 0.2f;
     private readonly float playerMovementSpeed = 2.5f;
     private readonly float jumpPower = 4.5f;
     private float horizontalInput;
-    private Rigidbody2D playerRB;
     private bool rightFace = true;
+    private bool isGrounded = false;
+    
     private void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
-        
+        level = GameObject.Find("Level").GetComponent<Level>();
+        groundCheck = GameObject.Find("GroundCheck").GetComponent<Transform>();
     }
-
     private void Update()
     {
+        GroundCheck();
         //Makes a float with the input "Horizontal" (Horizontal contains: A and LeftArrow for negative x movement; D and RightArrow for positive x movement)
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        playerRB.velocity = new Vector2(horizontalInput * playerMovementSpeed, playerRB.velocity.y);
-        
+
+        if (Physics2D.gravity == new Vector2(9.81f, 0f)) 
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, horizontalInput * playerMovementSpeed);
+        }
+        else if (Physics2D.gravity == new Vector2(-9.81f, 0f))
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, horizontalInput * -playerMovementSpeed);
+        }
+        else
+        {
+            playerRB.velocity = new Vector2(horizontalInput * playerMovementSpeed, playerRB.velocity.y);
+        }
+
         //Makes the play jump with the input: spacebar and W
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && isGrounded == true )
         {
             Jump();
         }
-  
         if (rightFace == false && horizontalInput > 0)
         {
             Mirror();
@@ -45,12 +53,22 @@ public class Player : MonoBehaviour
             Mirror();
         }
     }
-    private void Rotate(string direction)
+    //Checks if the invisible circle underneath the player is touching a layer named "Ground"
+    private void GroundCheck()
+    {
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, checkCollisionRadius, groundLayer);
+        if (colliders.Length > 0)
+        {
+            isGrounded = true;
+        }
+    }
+    //Rotates the play in a specific direction
+    public void Rotate(string direction)
     {
         if(direction == "Up")
         {
             transform.eulerAngles = new Vector3(0f, 0f, 180f);
-            
         }
         else if(direction == "Down")
         {
@@ -68,7 +86,6 @@ public class Player : MonoBehaviour
         {
             transform.eulerAngles = Vector3.zero;
         }
-       
     }
      //The player will face the correct direction
     private void Mirror() 
@@ -81,62 +98,38 @@ public class Player : MonoBehaviour
    
     private void Jump()
     {
-        playerRB.velocity = new Vector2(playerRB.velocity.x, jumpPower) ;
-    }
 
+        if (Physics2D.gravity == new Vector2(9.81f, 0f))
+        {
+            playerRB.velocity = new Vector2(-jumpPower, playerRB.velocity.y );
+        }
+        else if (Physics2D.gravity == new Vector2(-9.81f, 0f))
+        {
+            playerRB.velocity = new Vector2(jumpPower, playerRB.velocity.y);
+        }
+        else { 
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpPower); 
+        }
+    }
     //Detects other obstacles
     void OnCollisionEnter2D(Collision2D other)
     {
-        switch (other.gameObject.name)
-        {
-            case "Ravine":
-                Die();
-                Debug.Log("I touched a ravine");
-                break;
+        level.PlayerCollision(other);
 
-            case "Spike":
-                Die();
-                Debug.Log("I touched a spike");
+        switch (other.gameObject.tag)
+        {
+            case "Ground":
                 break;
         }
     }
-    //Checks for trigger to change the camera position
+   //Checks for trigger to change the camera position
     void OnTriggerEnter2D(Collider2D other)
     {
-        switch (other.gameObject.name)
-        {
-            case "1to2":
-                segmentCamera.transform.position = segmentCameraPositions[1];
-                break;
-        }
-
-        //Check gravity zone
-        switch (other.gameObject.name)
-        {
-            case "GravityUp":
-                Physics2D.gravity = new Vector2(0f, 9.81f);
-                Rotate("Up");
-                break;
-
-            case "GravityDown":
-                Physics2D.gravity = new Vector2(0f, -9.81f);
-                Rotate("Down");
-                
-                break;
-
-            case "GravityRight":
-                Physics2D.gravity = new Vector2(9.81f, 0f);
-                Rotate("Right");
-                break;
-
-            case "GravityLeft":
-                Physics2D.gravity = new Vector2(-9.81f, 0f);
-                Rotate("Left");
-                break;
-        }
+        level.PlayerCollider(other);
     }
+
     //Kills the player (Will respawn player soon)
-    private void Die()
+    public void Die()
     {
         Destroy(gameObject);
     }
