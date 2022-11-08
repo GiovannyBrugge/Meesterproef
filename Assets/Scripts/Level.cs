442b04c8f8d35f907d3cbd7a99153921c0239ebe
GiovannyBrugge <giovannybrugge@live.nl>
2022-11-08 16:53:05 +0100

    Added mute and unmute button + home button
    Added a mute and unmute button for the sounds and a back to home button

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,6 +9,8 @@ public class Level : MonoBehaviour
     //Checkpoint inactive sprite
     public Sprite checkpointInactive;
     //List of segment camera positions
+    public GameObject muteButton;
+    public GameObject unmuteButton;
     public readonly Vector3[] segmentCameraPositions =
     {
         //Segment 1
@@ -30,12 +32,14 @@ public class Level : MonoBehaviour
         //Segment 9
         new Vector3(0f,-11f,-10f)
     };
-
+    public static GameObject Canvas;
     public int currentActiveCheckpoint; //0 = spawn, 1 = checkpoint1, 2 = checkpoint2
     private bool gameWon = false;
+
     private void Start()
     {
         segmentCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        ResetPlayer();
         AudioManager.instance.Play("GameTheme1");
     }
     //Detects other obstacles
@@ -138,11 +142,11 @@ public class Level : MonoBehaviour
         else if (checkpoint == 1 && currentActiveCheckpoint != 2)
         {
             StopAllSoundtracks();
-            AudioManager.instance.Play("GameTheme3");
             AudioManager.instance.Play("Checkpoint");
             currentActiveCheckpoint = 2;
             InactiveAllCheckpoints();
             GameObject.Find("Checkpoint2").GetComponent<SpriteRenderer>().sprite = checkpointActive;
+            AudioManager.instance.Play("GameTheme3"); 
         }
     }
     //Stops all game themes
@@ -151,6 +155,26 @@ public class Level : MonoBehaviour
         AudioManager.instance.Stop("GameTheme1");
         AudioManager.instance.Stop("GameTheme2");
         AudioManager.instance.Stop("GameTheme3");
+    }
+    //Mute all sounds
+    public void MuteSoundtrack()
+    {
+        AudioListener.volume = 0;
+        muteButton.SetActive(false);
+        unmuteButton.SetActive(true);
+    }
+    //Unmute all sounds
+    public void UnmuteSoundtrack()
+    {
+        AudioListener.volume = 1;
+        unmuteButton.SetActive(false);
+        muteButton.SetActive(true);
+    }
+    //Go back to main menu
+    public void GoToMainMenu()
+    {
+        StopAllSoundtracks();
+        SceneManager.LoadScene(0);
     }
     //Sets all checkpoints inactive by default
     private void InactiveAllCheckpoints()
@@ -173,5 +197,12 @@ public class Level : MonoBehaviour
                 gameWon = false;
                 break;
         }
+    }
+    //Resets all player info
+    private void ResetPlayer()
+    {
+        segmentCamera.transform.position = segmentCameraPositions[0];
+        Physics2D.gravity = new Vector2(0f, -9.81f);
+        AudioListener.volume = 1;
     }
 }
